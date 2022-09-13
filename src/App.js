486c576a244f2acdbbbc59c106e6f35f3826fece
“vvela2022” <victoriavela01@gmail.com>
2022-09-13 15:05:52 -0500

    updated header with styling and links

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
+//IMPORT COMPONENTS
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+import {Route, Routes} from 'react-router-dom'
+
+//IMPORT PAGES
+import About from './pages/About'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+
 function App() {
+const URL = 'https://portfolio-vv.herokuapp.com/'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/about' element={<About URL={URL} />}/>
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import {Link} from 'react-router-dom'
+
+const Header = (props) => {
+//inline styling
+const navStyle = {
+    display: 'flex',
+    justifyContent: 'space-around',
+    border: '3px solid black',
+    padding: '8px',
+    width: '90%',
+    margin: 'auto',
+}
+
+    return(
+        <header class='header-container'>
+            <h1>My Portfolio Page</h1>
+            <nav style= {navStyle}>
+                <Link to='/'>
+                    <div>Home</div>
+                </Link>
+                <Link to='/about'>
+                    <div>About</div>
+                </Link>
+                <Link to='/projects'>
+                    <div>Projects</div>
+                </Link>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
