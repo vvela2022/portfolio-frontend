4b6919b8713f0a64e0f99bd11e8d79b6fff891b4
“vvela2022” <victoriavela01@gmail.com>
2022-10-13 17:58:21 -0500

    changed navbar header from home to about

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,7 +21,7 @@ const Header = (props) => {
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                        <Link to='/' class="nav-link active" aria-current="page">Home</Link>
+                        <Link to='/' class="nav-link active" aria-current="page">About</Link>
                         </li>
                         <li class="nav-item">
                         <Link to='/about' class="nav-link active" aria-current="page">Contact</Link>
