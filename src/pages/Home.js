afd2de08bcf65cbda07c9a9c8decd49dbbf2100a
“vvela2022” <victoriavela01@gmail.com>
2022-10-17 13:21:32 -0500

    added background image and adjusted styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,20 +5,25 @@ import Footer from '../components/Footer'
 const Home = (props) => {
     return (
     <div class="home-container">
-        <div class="hello">
-            <h1 class="home-title">Victoria Vela</h1>
-            <h4 class="home-subtitle">Software Engineer</h4>
-        </div>
-        <div class="container text-center information">
-            <div class="row welcome-container">
-                <div class="col profile-pic">
+        
+        <div class="information">
+            <div class="welcome-container">
+                <div class="hello">
+                    <h1 class="home-title">Victoria Vela</h1>
+                    <h4 class="home-subtitle">Software Engineer</h4>
+                </div>
+                <div class="pic-info">
+                <div class="profile-pic">
                     <img class="profile-img" src="https://i.imgur.com/35BIyXH.jpg?2" alt="Victoria Vela"></img>
                 </div>
-                <div class="col info-container">
+                <div class="info-container">
                     <h4 class="home-headline">A Bit About Me</h4>
                     <p class="home-text">I’m a software engineer with a background in sales who is energized by collaborating with team members to solve complex problems.
 I have a strong passion for improving user experience and am energized by delivering technology based solutions.</p>
                 </div>
+
+                </div>
+                
             </div>
         </div> 
      <Tech />
