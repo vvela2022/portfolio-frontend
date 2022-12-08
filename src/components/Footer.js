06b87019225a9d8f733ae55966ce38ea37df1c78
“vvela2022” <victoriavela01@gmail.com>
2022-12-08 11:16:47 -0600

    updating resume attempt 2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import resume from '../assets/files/Resume_VictoriaVela.pdf'
+import resume from '../assets/files/Resume-VictoriaVela.pdf'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
